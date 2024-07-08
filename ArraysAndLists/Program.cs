be91ac50f9b9453bfebdb605cbc599fb4abe707e
Kyle Cleary <kscleary11@gmail.com>
2024-07-08 16:31:49 -0600

    Exercise Complet

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,36 +10,65 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int array and populate it with numbers 1-10
-            
+            int[] oneToTen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create a list of type int
              * Name the list "evens"
              */
-
-            
+            var evens = new List<int>();
             /* Create another list of type int
              * Name the list "odds"
              */
+            var odds = new List<int>();
+
+            /* Using either a foreach or for loop,
+            * iterate through the array you populated with 10 numbers.
+            * Inside the scope of the loop,
+            * check to see if each number in the array is even or odd.
+            * If the number is even, add it to the evens list.
+            * If the number is odd, add it to the odds list.
+            */
+            for (int i = 1; i <= oneToTen.Length; i++)
+            {
+
+                Console.WriteLine(i);
+                if (i % 2 == 0)
+
+                {
+                    evens.Add(i);
+                }
+                else
+                {
+                    odds.Add(i);
+                }
+
+            }
+
+
+
+
+
+
+                /* Using a foreach loop,
+                 * display the numbers in your "evens" list
+                 */
+                Console.WriteLine("Evens:");
+                    foreach (var num in evens)
+                {
+                    Console.WriteLine(num);
+                }
+                Console.WriteLine("Odds:");
+                
+                    foreach (var num in odds)
+            {
+                Console.WriteLine(num);
+            }
+
+                    /* Using a for loop,
+                     * display the numbers in your "odds" list
+                     */
+                
             
-
-             /* Using either a foreach or for loop,
-             * iterate through the array you populated with 10 numbers.
-             * Inside the scope of the loop,
-             * check to see if each number in the array is even or odd.
-             * If the number is even, add it to the evens list.
-             * If the number is odd, add it to the odds list.
-             */
-
-
-
-            /* Using a foreach loop,
-             * display the numbers in your "evens" list
-             */
-            
-
-            /* Using a for loop,
-             * display the numbers in your "odds" list
-             */
         }
     }
 }
